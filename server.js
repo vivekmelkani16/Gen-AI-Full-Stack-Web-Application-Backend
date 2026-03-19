2a5abb44b50bab6c31a1c4b1bc0ce8c1c2a09988
vivekmelkani <melkanivivek7@gmail.com>
2026-03-19 17:23:02 +0530

    fix db connection logging

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
 app.get("/", (req, res) => {
-  res.send("Backend is running on Vercel 🚀");
+    res.send("Backend is running on Vercel 🚀");
 });
 
 connectToDB()
+    .then(() => console.log("DB connected"))
+    .catch(err => {
+        console.error("DB connection failed:", err);
+});
 
 
 module.exports = app;
