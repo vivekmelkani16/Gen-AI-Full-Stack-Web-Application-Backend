33c1e3694bf009066df10c3bd4baad3cbd435ff3
vivekmelkani <melkanivivek7@gmail.com>
2026-03-19 17:12:06 +0530

    fix cors and deployment

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
+app.get("/", (req, res) => {
+  res.send("Backend is running on Vercel 🚀");
+});
+
 connectToDB()
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+module.exports = app;
--- src/app.js
+++ src/app.js
@@ -7,8 +7,11 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://your-frontend.vercel.app" // 👈 add your frontend link here
+  ],
+  credentials: true
 }))
 
 /* require all the routes here */
@@ -19,7 +22,6 @@ const interviewRouter = require("./routes/interview.routes")
 /* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
-
 
 
 module.exports = app
